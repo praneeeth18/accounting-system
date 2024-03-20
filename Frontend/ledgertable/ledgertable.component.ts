aea406abaf1547cc113470fe51abec2ca62f9a62
Uswaroop <132978717+Uswaroop@users.noreply.github.com>
2024-03-21 00:10:00 +0530

    ledger table

---
+++ Frontend/ledgertable/ledgertable.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ledgertable',
+  templateUrl: './ledgertable.component.html',
+  styleUrl: './ledgertable.component.css'
+})
+export class LedgertableComponent {
+
+}
